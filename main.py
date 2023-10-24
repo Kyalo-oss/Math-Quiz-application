218f504abc2d531c83fe9370b31f2321e02a53d7
Steve Kyalo <kyalosteve642@gmail.com>
2023-10-24 16:06:36 +0300

    Added Addition

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ import random
 continue_playing = "yes"
 
 while continue_playing == "yes":
-    num1 = random.randint(1, 20)
-    num2 = random.randint(1, 20)
-    question = f"What is {num1} x {num2}?"
-    answer = int(input(question))
-    if answer == num1 * num2:
+    num1 = random.randint(1, 12)
+    num2 = random.randint(1, 12)
+    operation = random.choice(["+", "x", "/"])
+    if operation == "+":
+        question = f"What is {num1} + {num2}?"
+        answer = num1 + num2
+    else:
+        question = f"What is {num1} x {num2}?"
+        answer = num1 * num2
+
+    user_answer = int(input(question))
+    if user_answer == answer:
         print("Correct!")
     else:
         print("Incorrect!")
     continue_playing = input("Do you want to continue? (yes or no) ")
 
 print("Thank you for taking the quiz!")
+
 
