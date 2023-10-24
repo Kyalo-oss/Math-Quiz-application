d6495ca89279dca094e818237baf84d99d0e2de2
Steve Kyalo <kyalosteve642@gmail.com>
2023-10-24 16:12:50 +0300

    Added Division and Subtraction

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ continue_playing = "yes"
 while continue_playing == "yes":
     num1 = random.randint(1, 12)
     num2 = random.randint(1, 12)
-    operation = random.choice(["+", "x", "/"])
+    operation = random.choice(["+", "-", "x", "/"])
     if operation == "+":
         question = f"What is {num1} + {num2}?"
         answer = num1 + num2
-    else:
+    elif operation == "-":
+        question = f"What is {num1} - {num2}?"
+        answer = num1 - num2
+    elif operation == "x":
         question = f"What is {num1} x {num2}?"
         answer = num1 * num2
-
-    user_answer = int(input(question))
+    else:
+        question = f"What is {num1} / {num2}?"
+        answer = num1 / num2
+    user_answer = float(input(question))
     if user_answer == answer:
         print("Correct!")
     else:
@@ -21,5 +26,6 @@ while continue_playing == "yes":
     continue_playing = input("Do you want to continue? (yes or no) ")
 
 print("Thank you for taking the quiz!")
+
 
 
